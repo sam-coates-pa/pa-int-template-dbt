2b619bdffbf24fbf4b82c8c43e1dbbf1391bce47
Sam Coates <138590259+sam-coates-pa@users.noreply.github.com>
2026-09-19 18:01:07 +0100

    Add SQL query to select sales order data

--- models/staging/sales/stg_sales.sql
+++ models/staging/sales/stg_sales.sql
@@ -1,1 +1,11 @@
+select
 
+    order_id,
+    customer_id,
+    product_id,
+    cast(order_date as date) as order_date,
+    cast(revenue as numeric(18,2)) as revenue,
+    upper(order_status) as order_status,
+    created_at
+
+from {{ source('sales', 'orders') }}
