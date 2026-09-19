6e7e1f181a54b14870693a7c60049196e2592ee6
Sam Coates <138590259+sam-coates-pa@users.noreply.github.com>
2026-09-19 17:58:02 +0100

    Add SQL query to select customer details

--- models/staging/customers/stg_customers.sql
+++ models/staging/customers/stg_customers.sql
@@ -1,1 +1,9 @@
+select
 
+    customer_id,
+    trim(customer_name) as customer_name,
+    upper(status) as customer_status,
+    created_at,
+    updated_at
+
+from {{ source('customers', 'customers') }}
