75cf12b0c3464bae09ea5de6eb734cf62cd8d986
Sam Coates <138590259+sam-coates-pa@users.noreply.github.com>
2026-09-19 18:03:46 +0100

    Add macro to get the latest record from a table

--- models/macros/get_latest_record.sql
+++ models/macros/get_latest_record.sql
@@ -1,1 +1,11 @@
+{% macro get_latest_record(table_name, date_column) %}
 
+select *
+
+from {{ table_name }}
+
+qualify row_number() over (
+    order by {{ date_column }} desc
+) = 1
+
+{% endmacro %}
