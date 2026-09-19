52db04cfdfdb8cdc34cff62bf634df026de9d692
Sam Coates <138590259+sam-coates-pa@users.noreply.github.com>
2026-09-19 18:03:34 +0100

    Create generate_surrogate_key macro
    Add a macro to generate a surrogate key using MD5.

--- models/macros/generate_surrogate_key.sql
+++ models/macros/generate_surrogate_key.sql
@@ -1,1 +1,12 @@
+{% macro generate_surrogate_key(columns) %}
 
+md5(
+    concat(
+        {% for column in columns %}
+            coalesce(cast({{ column }} as string), '')
+            {% if not loop.last %}, '|' ,{% endif %}
+        {% endfor %}
+    )
+)
+
+{% endmacro %}
