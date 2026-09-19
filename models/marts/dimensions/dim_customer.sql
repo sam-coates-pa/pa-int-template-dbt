453a76f58423e3d48cfddfe95eef92ffc33679b2
Sam Coates <138590259+sam-coates-pa@users.noreply.github.com>
2026-09-19 18:02:33 +0100

    Add customer data selection with joins

--- models/marts/dimensions/dim_customer.sql
+++ models/marts/dimensions/dim_customer.sql
@@ -1,1 +1,21 @@
+select
 
+    {{ generate_surrogate_key(['c.customer_id']) }}
+        as customer_key,
+
+    c.customer_id,
+    c.customer_name,
+    c.customer_status,
+
+    o.order_count,
+
+    r.total_revenue,
+    r.average_order_value
+
+from {{ ref('stg_customers') }} c
+
+left join {{ ref('int_customer_orders') }} o
+    on c.customer_id = o.customer_id
+
+left join {{ ref('int_customer_revenue') }} r
+    on c.customer_id = r.customer_id
