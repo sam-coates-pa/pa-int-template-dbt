c12f24edc0c014fc71fad651a104c12ed33385ae
Sam Coates <138590259+sam-coates-pa@users.noreply.github.com>
2026-09-19 18:04:14 +0100

    Add customer_snapshot SQL for customer data

--- models/snapshots/customer_snapshot.sql
+++ models/snapshots/customer_snapshot.sql
@@ -1,1 +1,19 @@
+{% snapshot customer_snapshot %}
 
+{{
+    config(
+        target_schema='snapshots',
+        strategy='check',
+        unique_key='customer_id',
+        check_cols=[
+            'customer_name',
+            'customer_status'
+        ]
+    )
+}}
+
+select *
+
+from {{ ref('stg_customers') }}
+
+{% endsnapshot %}
